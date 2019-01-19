8735a6eea8a4a59c3be78f49e562aa029290e840
iDarkDuck <nevinganesan@hotmail.com>
2019-01-18 22:30:33 -0800

    Initial Commit

---
+++ GitBashMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitBashMac
+//
+//  Created by iDarkDuck on 2019-01-18.
+//  Copyright © 2019 iDarkDuck. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
