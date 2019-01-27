0355c9a05653c0767d318cb97804b10853ca26e0
iDarkDuck <nevinganesan@hotmail.com>
2019-01-26 20:36:36 -0500

    App Logic Working and App Icon

--- GitBashMac/AppDelegate.swift
+++ GitBashMac/AppDelegate.swift
@@ -11,39 +11,12 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-        if let button = statusItem.button {
-            button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
-            button.action = #selector(printQuote(_:))
-        }
-        constructMenu()
+        //constructStatusIem()
     }
     
-    func constructMenu() {
-        let menu = NSMenu()
-        
-        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "P"))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-        
-        statusItem.menu = menu
-    }
-
-
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
     
-    @objc func printQuote(_ sender: Any?) {
-        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
-        let quoteAuthor = "Mark Twain"
-
-        print("\(quoteText) — \(quoteAuthor)")
-    }
-
 }
 
--- GitBashMac/ViewController.swift
+++ GitBashMac/ViewController.swift
@@ -9,16 +9,23 @@
 import Cocoa
 import Foundation
 
-
 class ViewController: NSViewController {
     
     @IBOutlet weak var enableButton: NSButton!
     
     
+    @IBOutlet var welcomeImage: NSImageView!
+    var enable = true;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        welcomeImage.image =  NSImage(named:"GitBashMac.png")!
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        self.view.window?.title = "GitBash for MacOS"
+        constructStatusIem()
     }
     
     override var representedObject: Any? {
@@ -27,36 +34,112 @@ class ViewController: NSViewController {
         }
     }
     
-
-    
-    @IBAction func enableButtonAction(_ sender: Any) {
-        let currentBranch = bash(command: "git", arguments: ["describe", "--contains", "--all", "HEAD"])
-        print("current branch:\(currentBranch)")
+    func setImage(){
+        let nsDocumentDirectory = FileManager.SearchPathDirectory.documentDirectory
+        let nsUserDomainMask    = FileManager.SearchPathDomainMask.userDomainMask
+        let paths               = NSSearchPathForDirectoriesInDomains(nsDocumentDirectory, nsUserDomainMask, true)
+        if let dirPath = paths.first{
+            let imageURL = URL(fileURLWithPath: dirPath).appendingPathComponent("GitBashMac.png")
+            let image    = NSImage(contentsOfFile: imageURL.path)
+            // Do whatever you want with the image
+            welcomeImage.image = image
+            print("success")
+        }
     }
     
+    @IBAction func enableButtonAction(_ sender: Any) {
+        var command = ""
+        var title = ""
+        enableButton.isEnabled = false
+        if(enable){
+            title = "Disable";
+            command = "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/fabriziocucci/git-bash-for-mac/master/install.sh)\""
+        } else{
+            enableButton.isEnabled = false
+            title = "Enable";
+            command = "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/fabriziocucci/git-bash-for-mac/master/uninstall.sh)\""
+        }
+        do{
+          try executeCommand(command, title)
+        } catch let error {
+            print("Error: \(error)")
+        }
+    }
     
-    func shell(launchPath: String, arguments: [String]) -> String{
+    func executeCommand(_ command: String, _ title: String) throws {
+        self.enableButton.title = "Loading..."
+        print(self.shell(command))
+        self.enable = !self.enable;
+        self.enableButton.title = title
+        self.enableButton.isEnabled = true
+        self.relaunchTerminal()
+        
+    }
+    
+    func relaunchTerminal(){
+        print(shell("osascript -e 'quit app \"Terminal\"'"))
+        print(shell("osascript -e 'tell application \"Terminal\" to activate'"))
+    }
+    
+    func shell(_ command: String) -> String {
         let task = Process()
-        task.launchPath = launchPath
-        task.arguments = arguments
+        task.launchPath = "/bin/bash"
+        task.arguments = ["-c", command]
         
         let pipe = Pipe()
         task.standardOutput = pipe
         task.launch()
         
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: String.Encoding.utf8)!
-        if output.characters.count > 0 {
-            //remove newline character.
-            let lastIndex = output.index(before: output.endIndex)
-            return String(output[output.startIndex ..< lastIndex])
-        }
+        let output: String = NSString(data: data, encoding: String.Encoding.utf8.rawValue)! as String
+        
         return output
     }
     
-    func bash(command: String, arguments: [String]) -> String {
-        let whichPathForCommand = shell(launchPath: "/bin/bash", arguments: [ "-l", "-c", "which \(command)" ])
-        return shell(launchPath: whichPathForCommand, arguments: arguments)
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    
+    func constructStatusIem() {
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+            button.action = #selector(printQuote(_:))
+        }
+        constructMenu()
     }
-
+    
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "- Minimize GitBashMac", action: #selector(ViewController.minimizeWindow(_:)), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem(title: "+ Maximize GitBashMac", action: #selector(ViewController.maximizeWindow(_:)), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit GitBashMac", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        statusItem.menu = menu
+    }
+    
+    @objc func printQuote(_ sender: Any?) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        print("\(quoteText) — \(quoteAuthor)")
+    
+    }
+    
+    @objc func minimizeWindow(_ sender: Any?){
+        NSApp.setActivationPolicy(.accessory)
+        // The application is an ordinary app that appears in the Dock and may
+        // have a user interface.
+        //NSApp.setActivationPolicy(.regular)
+        
+        // The application does not appear in the Dock and does not have a menu
+        // bar, but it may be activated programmatically or by clicking on one
+        // of its windows.
+        //NSApp.setActivationPolicy(.accessory)
+        
+        // The application does not appear in the Dock and may not create
+        // windows or be activated.
+        //NSApp.setActivationPolicy(.prohibited)
+    }
+    
+    @objc func maximizeWindow(_ sender: Any?){
+        NSApp.setActivationPolicy(.regular)
+    }
 }
